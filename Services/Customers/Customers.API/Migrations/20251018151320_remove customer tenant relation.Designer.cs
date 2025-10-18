185148191ccc4fb793e7e77ed6907638025bfe78
Ahmed Salah <142540119+Ahmed-Saalah@users.noreply.github.com>
2025-10-18 18:14:14 +0300

    Add New Customers Migrations V2

--- Services/Customers/Customers.API/Migrations/20251018151320_remove customer tenant relation.Designer.cs
+++ Services/Customers/Customers.API/Migrations/20251018151320_remove customer tenant relation.Designer.cs
@@ -4,6 +4,7 @@ using Customers.API.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Customers.API.Migrations
 {
     [DbContext(typeof(CustomersDbContext))]
-    partial class CustomersDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251018151320_remove customer tenant relation")]
+    partial class removecustomertenantrelation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
