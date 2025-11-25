1dec38256cb481200f00f97021801121a1d64787
Ahmed Salah <142540119+Ahmed-Saalah@users.noreply.github.com>
2025-11-25 23:22:23 +0200

    Add Create Store Endpoint

---
+++ Services/Store/Store.API/Endpoints/CreateStoreEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Web.Helpers;
+using Store.Core.Features;
+
+namespace Store.API.Endpoints;
+
+public sealed class CreateStoreEndpoint
+{
+    public sealed class Endpoint : Extensions.IEndpoint
+    {
+        public void Map(IEndpointRouteBuilder app)
+        {
+            app.MapPost(
+                    "/api/stores",
+                    async (
+                        [FromServices] IMediator mediator,
+                        [FromRoute] int customerId,
+                        [FromBody] CreateStore.Request data
+                    ) =>
+                    {
+                        var response = await mediator.Send(data);
+                        return response.ToHttpResult();
+                    }
+                )
+                .WithTags("Customer Address");
+        }
+    }
+}
